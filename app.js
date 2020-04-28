e57dbb39e5056814b5488ad39c10344aa1c0c9df
Sang Nguyen <sangnn2012@gmail.com>
2020-04-28 10:19:21 +0700

    Refactor geocode

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const request = require('request')
-
-const url = 'http://api.weatherstack.com/current?access_key=188ced6669489eee2874ce72ea39662a&query=37.8,-122.4&units=f'
-
+const geocode = require('./utils/geocode')
 
 
-request({ url: url, json: true}, (error, response) => {
-    if(error){
-        console.log("Unable to connect to weather service!");
-    } else {
-        console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out")
-    }
-})
+// const url = 'http://api.weatherstack.com/current?access_key=188ced6669489eee2874ce72ea39662a&query=37.8,-122.4&units=f'
 
-// Geocoding
-// const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibmlnaHR3YXk3IiwiYSI6ImNrOTlyMnljNjAzeTEzZ29sa2U0bjJueTUifQ.To3thxAYmzdbhHAbzPJ_2w&limit=1"
-
-
-// Address -> Lat/Long -> Weather
-// request({ url: geocodeURL, json: true}, (error, response) => {
-//     const longitude = response.body.features[0].center[0]
-//     const latitude = response.body.features[0].center[1]
-
-//     console.log(longitude, latitude);
+// request({ url: url, json: true}, (error, response) => {
+//     if(error){
+//         console.log("Unable to connect to weather service!");
+//     } else {
+//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees outside.")
+//     }
 // })
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const forecast = (longitude, latitude, callback) => {
+
+}
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
