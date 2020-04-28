749f49bbaeafa841035a9b7414744ad0c636a347
Sang Nguyen <sangnn2012@gmail.com>
2020-04-28 14:37:27 +0700

    Refactor code by using object shorthand syntax

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-const request = require('request')
 const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
-// const url = 'http://api.weatherstack.com/current?access_key=188ced6669489eee2874ce72ea39662a&query=37.8,-122.4&units=f'
+if (!address) return console.log('Please provide us a location!')
 
-// request({ url: url, json: true}, (error, response) => {
-//     if(error){
-//         console.log("Unable to connect to weather service!");
-//     } else {
-//         console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees outside.")
-//     }
-// })
-//
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-const forecast = (longitude, latitude, callback) => {
-
-}
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+geocode(address, (error, { latitude, longitude, location })=>{
+    if(error) return console.log(error)
+    forecast(latitude, longitude, (error, forecastData) => {
+        if(error) return console.log(error)
+        console.log(location)
+        console.log(forecastData)
+    })
 })
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibmlnaHR3YXk3IiwiYSI6ImNrOTlyMnljNjAzeTEzZ29sa2U0bjJueTUifQ.To3thxAYmzdbhHAbzPJ_2w&limit=1'
 
-    request({url: url, json: true}, (error, response)=>{
+    request({url, json: true}, (error, { body })=>{
         if(error){
             callback('Unable to connect to location services!', undefined)
-        } else if(response.body.features.length === 0){
+        } else if(body.features.length === 0){
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
-                longitude : response.body.features[0].center[0],
-                latitude : response.body.features[0].center[1],
-                location : response.body.features[0].place_name
+                longitude : body.features[0].center[0],
+                latitude : body.features[0].center[1],
+                location : body.features[0].place_name
             })
         }
     })
